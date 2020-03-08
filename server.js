d31ed24a61444f6cab04251a5f327d20ec213a92
Wei <wei.liao@msa.hinet.net>
2020-03-08 15:36:17 +0800

    fix css problem

--- server.js
+++ server.js
@@ -3,19 +3,17 @@ const path = require("path");
 
 const app = express();
 
-console.log(process.env.NODE_ENV);
-
-// if (process.env.NODE_ENV !== "production") {
-//   const webpackMiddleware = require("webpack-dev-middleware");
-//   const webpack = require("webpack");
-//   const webpackConfig = require("./webpack.config");
-//   app.use(webpackMiddleware(webpack(webpackConfig)));
-// } else {
-app.use(express.static("bundle"));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "bundle/index.html"));
-});
-// }
+if (process.env.NODE_ENV !== "production") {
+  const webpackMiddleware = require("webpack-dev-middleware");
+  const webpack = require("webpack");
+  const webpackConfig = require("./webpack.config");
+  app.use(webpackMiddleware(webpack(webpackConfig)));
+} else {
+  app.use(express.static("bundle"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "bundle/index.html"));
+  });
+}
 
 app.listen(process.env.PORT || 3030, () => {
   console.log("Listening");
